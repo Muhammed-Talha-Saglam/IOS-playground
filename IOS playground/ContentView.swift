d3d4b950525b20b16294041f8f98b4cc4b6316b7
talha.saglam <1699.mTs>
2023-01-22 18:44:01 +0300

    Added LottieView

--- IOS playground/ContentView.swift
+++ IOS playground/ContentView.swift
@@ -10,7 +10,12 @@ import SwiftUI
 struct ContentView: View {
 
     var body: some View {
-        FlightAppContentView()
+        
+        ZStack(alignment: .center){
+            MarqueeText(text: "This is a sample text for the marquee effect.", duration: 5)
+                .frame(width: 300, height: 50)
+        
+        }
     }
     
 }
@@ -21,3 +26,27 @@ struct ContentView_Previews: PreviewProvider {
     }
 }
 
+struct MarqueeText: View {
+    let text: String
+    let duration: Double
+    @State private var offset: CGFloat = 0
+    var body: some View {
+        GeometryReader { geometry in
+            Text(self.text)
+                .lineLimit(1)
+                .offset(x: self.offset, y: 0)
+                .onAppear {
+                    withAnimation(Animation.linear(duration: self.duration).repeatForever(autoreverses: false)) {
+                        self.offset = -geometry.size.width
+                    }
+                }
+        }
+    }
+}
+
+//func textSize()-> CGFloat {
+//    let attributes = [NSAttributedString.Key.font: font]
+//    let size = (text as NSString).size(withAttributes: attributes)
+//
+//    return size.width
+//}
