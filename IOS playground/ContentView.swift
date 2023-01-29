f2a06f1b9625182eaea3319fe5d029fe4b27e684
talha.saglam <1699.mTs>
2023-01-29 19:24:51 +0300

    Added OffsetY extension to View.

--- IOS playground/ContentView.swift
+++ IOS playground/ContentView.swift
@@ -10,11 +10,8 @@ import SwiftUI
 struct ContentView: View {
 
     var body: some View {
-        
-        ZStack(alignment: .center){
-            MarqueeText(text: "This is a sample text for the marquee effect.", duration: 5)
-                .frame(width: 300, height: 50)
-        
+        ZStack {
+            
         }
     }
     
@@ -25,28 +22,3 @@ struct ContentView_Previews: PreviewProvider {
         ContentView()
     }
 }
-
-struct MarqueeText: View {
-    let text: String
-    let duration: Double
-    @State private var offset: CGFloat = 0
-    var body: some View {
-        GeometryReader { geometry in
-            Text(self.text)
-                .lineLimit(1)
-                .offset(x: self.offset, y: 0)
-                .onAppear {
-                    withAnimation(Animation.linear(duration: self.duration).repeatForever(autoreverses: false)) {
-                        self.offset = -geometry.size.width
-                    }
-                }
-        }
-    }
-}
-
-//func textSize()-> CGFloat {
-//    let attributes = [NSAttributedString.Key.font: font]
-//    let size = (text as NSString).size(withAttributes: attributes)
-//
-//    return size.width
-//}
--- IOS playground/Extensions/ExtensionsView.swift
+++ IOS playground/Extensions/ExtensionsView.swift
@@ -24,4 +24,43 @@ extension View {
         return UIScreen.main.bounds
     }
     
+    //Mark: Previous, current offset to find the direction of swipe
+    @ViewBuilder
+    func offsetY(completion: @escaping (CGFloat, CGFloat)->()) -> some View {
+        self
+            .modifier(OffsetHelper(onChange: completion))
+    }
+    
 }
+
+// Mark: Offset Helper. https://www.youtube.com/watch?v=heVnQvJbd4I
+struct OffsetHelper: ViewModifier {
+    var onChange: (CGFloat, CGFloat) -> ()
+    
+    @State var currentOffset: CGFloat = 0
+    @State var previousOffset: CGFloat = 0
+
+    func body(content: Content) -> some View {
+        content
+            .overlay {
+                GeometryReader { proxy in
+                    let minY = proxy.frame(in: .global).minY
+                    Color.clear
+                        .preference(key: OffsetKey.self, value: minY)
+                        .onPreferenceChange(OffsetKey.self) { value in
+                            previousOffset = currentOffset
+                            currentOffset = value
+                            onChange(previousOffset,currentOffset)
+                        }
+                }
+            }
+    }
+}
+
+//// Mark: Offset key
+//struct OffsetKey: PreferenceKey {
+//    static var defaultValue: CGFloat = 0
+//    static func reduce(value: inout CGFloat, nextValue: () -> CGFloat) {
+//        value = nextValue()
+//    }
+//}
